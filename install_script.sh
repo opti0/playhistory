e95a9e636331d6624f514eb28337d3df4b046632
dietpawel <pawel.dietrich@gmail.com>
2018-10-20 23:38:43 +0200

    Add debian install script.

---
+++ install_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#to be run as root in debian OS
+
+#exit on error
+set -e
+
+#install python3 and its module manager.
+apt install python3 python3-pip -y
+#using pip3 install virtual enviroment
+pip3 install virtualenv
+
+#make virtualenviroment with python3 interpreter
+virtualenv app/venv -p $(which python3)
+
+#install requirements
+app/venv/bin/pip3 install -r pip3req.txt
+
+# make database
+app/venv/bin/python3 app/db.py create
+
+echo 'Install script finished successfully!'
